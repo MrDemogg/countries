e08733829535f7639fe4e07278586abf7d02ca05
demog <dg08gg@mail.ru>
2022-08-27 16:34:37 +0300

    (Ошибка в прошлом commit на этот файл, переписываю commit) API файл, дающий названия всех стран с сервера

--- src/API/CountryNameService.js
+++ src/API/CountryNameService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default class CountryNameService {
   static async getAll() {
     const response = await axios.get('https://restcountries.com/v3.1/all?fields=name')
+    console.log(response)
     return [response.data, response]
   }
 }
