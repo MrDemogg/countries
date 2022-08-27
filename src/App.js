ce0dc8710a23aa21961ed38ce325b54a5364c30d
demog <dg08gg@mail.ru>
2022-08-27 16:31:58 +0300

    Вставил функции взятия с сервера названия стран, а потом взятия информации для каждой страны. Также от туда активируется главный файл (если не считать App.js), который рендерит весь сайт, так сказать. Хранит все данные.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
 import './App.css';
+import CountriesMain from "./components/CountriesMain";
+import {useEffect, useState} from "react";
+import CountryNameService from "./API/CountryNameService";
+import CountryAllService from "./API/CountryAllService";
 
 function App() {
+  const [countriesList, setCountriesList] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState('')
+  const [error, setError] = useState(false)
+  const [errorText, setErrorText] = useState('')
+  const [selectedCountryInfo, setSelectedCountryInfo] = useState({
+    flag: '',
+    capital: '',
+    population: 0,
+    cca2: '',
+    cca3: '',
+    borders: []
+  })
+  const getCountries = async () => {
+    const [gettedCountries, response] = await CountryNameService.getAll()
+    setCountriesList(gettedCountries)
+    console.log(response)
+    return response
+  }
+  const getCountriesAll = async (country) => {
+    country = country.split(',')[0].replace(/ /g, '%20')
+    const gettedCountry = await CountryAllService.getAll(country)
+    if (gettedCountry[0].borders) {
+      setSelectedCountryInfo({
+        flag: gettedCountry[0].flags.svg,
+        capital: gettedCountry[0].capital,
+        population: gettedCountry[0].population,
+        cca2: gettedCountry[0].cca2,
+        cca3: gettedCountry[0].cca3,
+        borders: gettedCountry[0].borders
+      })
+    } else {
+      setSelectedCountryInfo({
+        flag: gettedCountry[0].flags.svg,
+        capital: gettedCountry[0].capital,
+        population: gettedCountry[0].population,
+        cca2: gettedCountry[0].cca2,
+        cca3: gettedCountry[0].cca3,
+        borders: []
+      })
+    }
+    return gettedCountry
+  }
+  useEffect(() => {
+    getCountries().then((response) => {
+      if (response.status === 200) {
+        setError(false)
+        setErrorText('')
+      }
+    }).catch((err) => {
+      setError(true)
+      setErrorText(`Ошибка: ${err.message}`)
+    })
+  }, [])
 
+  return (
+    <CountriesMain
+      selectedCountry={selectedCountry}
+      setSelectedCountry={setSelectedCountry}
+      countriesList={countriesList}
+      setCountriesList={setCountriesList}
+      error={error}
+      errorText={errorText}
+      selectedCountryInfo={selectedCountryInfo}
+      getCountriesAll={getCountriesAll}
+    ></CountriesMain>
+  )
 }
 
 export default App;
